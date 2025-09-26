5164db7cd5e36a6744986f84d86c65954377033f
LUCYFHER\Admin <plocienniczak.konik@gmail.com>
2025-09-26 19:30:30 +0200

    Strona główna z logo i zdjęciami postów

--- wp-content/themes/pikademia/functions.php
+++ wp-content/themes/pikademia/functions.php
@@ -4,3 +4,21 @@ function add_theme_scripts() {
     wp_enqueue_script( 'script', get_template_directory_uri() . '/index.js', array(), filemtime(get_template_directory().'/index.js'), true );
 }
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+
+
+function theme_setup(){
+   
+    /** post thumbnail **/
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'custom-background' );
+    add_theme_support( 'custom-logo', array(
+        'height'               => 100,
+        'width'                => 400,
+        'flex-height'          => true,
+        'flex-width'           => true,
+        'header-text'          => array( 'site-title', 'site-description' ),
+        'unlink-homepage-logo' => true,
+    )  );
+
+}
+add_action('after_setup_theme','theme_setup');
--- wp-content/themes/pikademia/index.php
+++ wp-content/themes/pikademia/index.php
@@ -2,5 +2,50 @@
 get_header();
 
 
+echo '<div class="container">';
 
+    if(function_exists( 'the_custom_logo')){
+        the_custom_logo();
+    }
+    if(have_posts()){
+        while(have_posts()){
+            the_post(); 
+            echo '<h1 class="page_title_w">' .get_the_title(). '</h1>';
+            echo '<p class="main_content_w">' .get_the_title(). '</p>';
+
+
+        }
+    }
+
+    echo '<h2 class="main_section_description_w">OSTATNIE WPISY</h2>';
+    echo '<div class="last_post_section_w">';
+    $args = array(
+        'post_type' => 'post',
+        'post_status' => 'publish',
+        'order' => 'DESC',
+        'posts_per_page' => -1
+    );
+
+    $the_query = new WP_Query($args);
+
+    if($the_query -> have_posts()){
+        while($the_query -> have_posts()){
+            $the_query -> the_post();
+            echo '<div class="last_post_item_w">';
+                echo '<a href="' .get_the_permalink().'">';
+                if (has_post_thumbnail()){
+                    the_post_thumbnail();
+                }            
+                echo '<a href="' .get_the_permalink().'">'.get_the_title().'</a>';
+                echo '<p>'.substr(get_the_excerpt(), 0, 120).'</p>';
+            echo '</div>';
+    
+        }
+    }
+
+    wp_reset_postdata();
+
+    echo '</div>';
+
+echo '</div>';
 get_footer();
